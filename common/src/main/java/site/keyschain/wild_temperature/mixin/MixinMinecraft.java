c335b7d61ec106c916a2866b67d56d42c7241495
keyschain <pikachurr0@pm.me>
2024-01-15 19:25:49 +0100

    project setup

---
+++ common/src/main/java/site/keyschain/wild_temperature/mixin/MixinMinecraft.java
@@ -0,0 +1,19 @@
+package site.keyschain.wild_temperature.mixin;
+
+import site.keyschain.wild_temperature.Constants;
+import net.minecraft.client.Minecraft;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(Minecraft.class)
+public class MixinMinecraft {
+    
+    @Inject(at = @At("TAIL"), method = "<init>")
+    private void init(CallbackInfo info) {
+        
+        Constants.LOG.info("This line is printed by an example mod common mixin!");
+        Constants.LOG.info("MC Version: {}", Minecraft.getInstance().getVersionType());
+    }
+}
---
+++ fabric/src/main/java/site/keyschain/wild_temperature/platform/FabricPlatformHelper.java
@@ -0,0 +1,24 @@
+package site.keyschain.wild_temperature.platform;
+
+import site.keyschain.wild_temperature.platform.services.IPlatformHelper;
+import net.fabricmc.loader.api.FabricLoader;
+
+public class FabricPlatformHelper implements IPlatformHelper {
+
+    @Override
+    public String getPlatformName() {
+        return "Fabric";
+    }
+
+    @Override
+    public boolean isModLoaded(String modId) {
+
+        return FabricLoader.getInstance().isModLoaded(modId);
+    }
+
+    @Override
+    public boolean isDevelopmentEnvironment() {
+
+        return FabricLoader.getInstance().isDevelopmentEnvironment();
+    }
+}
